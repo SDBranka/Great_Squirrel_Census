16d768753df44f10d0df7d80057f116989252a38
Sam Branka <sdbranka@me.com>
2022-12-23 23:21:24 -0500

    completed

---
+++ README.md
@@ -0,0 +1,36 @@
+# Great Squirrel Census
+
+##### Table of Contents
+
+- [Description](#description)
+- [How To Use](#how-to-use)
+- [References](#references)
+
+---
+
+## Description
+
+Create a csv named squirrel_count.csv that shows the number of squirrels of each fur color from the squirrel_data.csv file.
+
+##### Technologies
+
+- Python
+- Pandas
+- Visual Studio
+
+---
+
+## How To Use
+
+Download or clone this repository to your desktop. Run main.py in an appropriate Python environment.
+
+---
+
+## References
+
+##### Continuing Work on
+
+- https://github.com/SDBranka/_100DOP_Exercises
+
+\
+[Back To The Top](#great-squirrel-census)
---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas
+
+
+# read in the squirrel_data.csv and store 
+#   it in a variable called squirrel_data
+squirrel_data = pandas.read_csv("squirrel_data.csv")
+
+# get the fur colors and the count of each
+squirrel_colors = squirrel_data["Primary Fur Color"].value_counts()
+# print(squirrel_colors)
+
+# convert the format to a dictionary
+squirrel_colors_dict = squirrel_colors.to_dict()
+# print(squirrel_colors_dict)
+
+# reformat the dictionary
+data_dict = {
+    "color": [color for color in squirrel_colors_dict.keys()],
+    "count": [count for count in squirrel_colors_dict.values()]
+}
+# print(data_dict)
+
+# create a dataframe from the data_dict dictionary
+data = pandas.DataFrame(data_dict)
+# write the dataframe to a csv file named squirrel_count.csv
+data.to_csv("squirrel_count.csv")
